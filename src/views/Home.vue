181a52b907b6e8ebf8bd96bd84ffb98fd4e45a3f
qingji <qing.ji@mobvoi.com>
2022-01-27 16:02:04 +0800

    github部署vue

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <ul>
+      <li>揪子</li>
+      <li>揪子</li>
+      <li>揪子</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+
+}
+</script>
