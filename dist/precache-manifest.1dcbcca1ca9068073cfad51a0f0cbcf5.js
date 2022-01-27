bbb30fb59c26f1bcbb03b32c59d27d72fabcaed1
qingji <qing.ji@mobvoi.com>
2022-01-27 17:47:13 +0800

    修改

--- dist/precache-manifest.1dcbcca1ca9068073cfad51a0f0cbcf5.js
+++ dist/precache-manifest.1dcbcca1ca9068073cfad51a0f0cbcf5.js
@@ -1,42 +1,42 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "7bcbe13990dc9450c995",
-    "url": "/css/app.cc834d47.css"
+    "url": "./css/app.cc834d47.css"
   },
   {
     "revision": "1b6132123712ff7508ac37addea7d840",
-    "url": "/favicoThree.ico"
+    "url": "./favicoThree.ico"
   },
   {
     "revision": "1a3b93e45d3001672b49b4a886c4740c",
-    "url": "/favicoTwo.ico"
+    "url": "./favicoTwo.ico"
   },
   {
     "revision": "82b9c7a5a3f405032b1db71a25f67021",
-    "url": "/img/logo.82b9c7a5.png"
+    "url": "./img/logo.82b9c7a5.png"
   },
   {
     "revision": "6ee7c100b6afb94c3f90a9d3da841ed2",
-    "url": "/index.html"
+    "url": "./index.html"
   },
   {
     "revision": "37dd4fe6442ed8544dcc",
-    "url": "/js/about.d10b798c.js"
+    "url": "./js/about.d10b798c.js"
   },
   {
     "revision": "7bcbe13990dc9450c995",
-    "url": "/js/app.5a06e7aa.js"
+    "url": "./js/app.5a06e7aa.js"
   },
   {
     "revision": "c02a513003d4e88bf295",
-    "url": "/js/chunk-vendors.a6867cca.js"
+    "url": "./js/chunk-vendors.a6867cca.js"
   },
   {
     "revision": "4dd2751dfa44c648270334e74aa5aa10",
-    "url": "/manifest.json"
+    "url": "./manifest.json"
   },
   {
     "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
-    "url": "/robots.txt"
+    "url": "./robots.txt"
   }
 ]);
